df315a6ef9d30a60c4dd40e3df9fc5e4544ceadd
Habiba Ferdausi Ritu <ritu.pramanik1@gmail.com>
2024-10-01 19:22:07 +0600

    dashboad layout added

---
+++ app/components/Dashboard/DashboardLayout.tsx
@@ -0,0 +1,19 @@
+import { useUser } from '@/app/lib/UserContext'
+import React from 'react'
+import Dashboard from './FreelancerDashboard/Dashboard';
+import DashboardCompany from './CompanyDashboard/DashboardCompany';
+
+export default function DashboardLayout() {
+    const {user} = useUser();
+  return (
+    <div>
+          {user && user.role === "freelancer" && (
+              <Dashboard/>     
+                  
+                )}
+                 {user && user.role === "company" && (
+          <DashboardCompany/>
+                )}
+    </div>
+  )
+}
