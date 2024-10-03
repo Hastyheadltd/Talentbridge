ec7f78065f21b192bc5da26c5d08bbf13f122b26
Habiba Ferdausi Ritu <ritu.pramanik1@gmail.com>
2024-10-03 23:26:40 +0600

    updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-        NEXT_PUBLIC_BASE_URL: "https://talent-bridge-server.vercel.app",
+        NEXT_PUBLIC_BASE_URL: "https://talent-bridge-server.onrender.com",
         },
         images: {
           remotePatterns: [
