8fb17fe41e4a7010510d6f6076c2bb5e1691154a
Habiba Ferdausi Ritu <ritu.pramanik1@gmail.com>
2025-03-05 16:04:44 +0600

    dashboard navbar added

--- app/components/Navbar/DashboardNav.tsx
+++ app/components/Navbar/DashboardNav.tsx
@@ -4,11 +4,12 @@ import { useUser } from "@/app/lib/UserContext";
 import { auth } from "@/firebase.config";
 import Image from "next/image";
 import Link from "next/link";
-import { usePathname, useRouter } from "next/navigation";
+import { useRouter } from "next/navigation";
 import Swal from "sweetalert2";
-
+import { IoIosNotificationsOutline } from "react-icons/io";
+import { RiArrowDropDownLine } from "react-icons/ri";
 export default function DashboardNav() {
-  const pathName = usePathname();
+ 
   const router = useRouter();
   const { user,logout } = useUser();
 
@@ -35,51 +36,26 @@ export default function DashboardNav() {
     }
   };
 
-  const navLinks = [
-    { path: "/dashboard", name: "Dashboard" },
-   
-    { path: "/dashboard/edit-profile", name: "Edit Profile" },
-    { path: "/dashboard/jobs", name: "Active Jobs" },
-    { path: "/dashboard/archive-jobs", name: "Archive Jobs" },
-    { path: "/dashboard/job-post", name: "Post A Job" },
-    { path: "/dashboard/applicants", name: "Applicants" },
-    { path: "/dashboard/messages", name: "Messages" },
-   
- 
-  ];
 
-  // Employer Navlinks
-  const EmpnavLinks = [
-    { path: "/dashboard", name: "Dashboard" },
-    { path: "/dashboard/edit-profile", name: "Edit Profile" },
-    { path: "/jobs", name: "Jobs" },
-    { path: "/dashboard/appliedjobs", name: "Applied Jobs" },
-    { path: "/dashboard/messages", name: "Messages" },
-    { path: "/dashboard/job-alert", name: "Job Alert" },
-    { path: "/dashboard/referral_program", name: "Referral Program" },
-  ];
 
   return (
-    <div className="bg-[#121420]">
-      <div className="max-w-[1440px] mx-auto h-[96px]">
-        <div className="navbar pt-6 flex justify-between items-center">
+    <div className="bg-white">
+      <div className="w-full mx-auto h-[80px] border-b border-black/10">
+        <div className="navbar px-10  py-3 flex justify-between items-center">
           {/* 1st part */}
           <div>
-            <Link href="/">
-              <img src="/logo.png" alt="logo" className="h-[50px]" />
-            </Link>
+           <h1 className="text-[32px] font-bold text-black ps-5 ">Dashboard</h1>
 
           </div>
 
           {/* 2nd part */}
-          <div className="flex justify-between gap-5 items-center">
-            <div className="bg-white w-[0.5px] h-[22px]"></div>
-
-            {/* Profile details with dropdown */}
-            <div className="dropdown dropdown-end">
+          <div className="flex justify-between gap-4 items-center">
+            <div className="w-[32px] h-[32px] rounded-full border flex justify-center items-center">
+            <IoIosNotificationsOutline size={20} /> 
+            </div>
          
-          <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
-            <div className="w-10 rounded-full">
+            <div className="bg-black/10 w-[0.5px] h-[25px] mx-2"></div>
+            <div className="flex items-center gap-2">
               <Image
                 src={
                   user?.photoURL
@@ -87,12 +63,23 @@ export default function DashboardNav() {
                     : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTmHkj6-Tndku8K2387sMaBf2DaiwfBtHQw951-fc9zzA&s"
                 }
                 alt="user img"
-                className="w-[56px] rounded-full h-[56px]"
+                className="w-[40px] rounded-full h-[40px]"
                 width={50}
                 height={50}
               />
-            </div>
+               <p className=" text-black text-[14px]  font-bold">{user?.username}</p>
+               <div>
+           
           </div>
+            </div>
+
+            {/* Profile details with dropdown */}
+            <div className="dropdown dropdown-end">
+         
+          <div tabIndex={0} role="button" className="w-5 h-5 border rounded-full flex justify-center items-center ">
+          <RiArrowDropDownLine size={24} />
+          </div>
+         
           <ul tabIndex={0} className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
             <li>
               <Link href="/" className="justify-between">
@@ -109,6 +96,7 @@ export default function DashboardNav() {
             </li>
           </ul>
         </div>
+       
           </div>
         </div>
       </div>
