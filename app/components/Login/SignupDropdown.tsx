daa670b9087c20d5f7e22cccdd0dd3ef18c6a5b5
Habiba Ferdausi Ritu <ritu.pramanik1@gmail.com>
2025-03-24 16:35:18 +0600

    login bugs issue fixed

--- app/components/Login/SignupDropdown.tsx
+++ app/components/Login/SignupDropdown.tsx
@@ -11,14 +11,14 @@ const SignupDropdown: React.FC = () => {
       </button>
 
       {/* Dropdown (Hidden by default, shown on hover) */}
-      <div className="absolute left-0  w-[100px] bg-blue-800  shadow-lg rounded-md opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-all duration-300">
+      <div className="absolute left-0  w-[100px] bg-white  shadow-lg rounded-md opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-all duration-300">
         <Link href="/signup/freelancer">
-          <p className="block px-4 py-2 text-[14px] text-gray-300 hover:text-white cursor-pointer">
+          <p className="block px-4 py-2 text-[14px] text-primary hover:text-blue-900 cursor-pointer">
             Freelancer
           </p>
         </Link>
         <Link href="/signup/company">
-          <p className="block px-4 py-2 text-[13px] text-gray-300 hover:text-white cursor-pointer">
+          <p className="block px-4 py-2 text-[13px] text-primary hover:text-blue-900  cursor-pointer">
             Company
           </p>
         </Link>
